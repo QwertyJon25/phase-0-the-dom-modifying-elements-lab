13541d8c54ae84eb1dc72fa94a4f85f6e88884d3
Jon Courtier <jonaqwerty@yahoo.com>
2021-08-20 15:13:59 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const testVar = "hello, world"
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Jon is the champion!";
+newHeader.className = "victory";
